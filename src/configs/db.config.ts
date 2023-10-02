d742c1c152bb11c036a891f1d585e9ba5ebfa622
tlevivsoft <97974662+tlevivsoft@users.noreply.github.com>
2023-10-01 23:09:10 -0700

    update workflow

--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -1,3 +1,3 @@
 export default () => ({
-  mongoUrl: parseInt(process.env.MONGO_URL),
+  mongoUrl: parseInt(process.env.MONGO_URL || `mongodb+srv://rm2:EwPSyrSol9FmPmb6@cluster0.2t16jzq.mongodb.net/rm2`),
 });
