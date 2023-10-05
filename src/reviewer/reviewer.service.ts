70e0842d6ed7bae871231e68bd30c13ac7212cde
tlevivsoft <97974662+tlevivsoft@users.noreply.github.com>
2023-10-05 00:47:30 -0700

    optimize search

--- src/reviewer/reviewer.service.ts
+++ src/reviewer/reviewer.service.ts
@@ -54,7 +54,8 @@ export class ReviewerService {
   async search({ name }: { name: string }): Promise<IReviewer[]> {
     const term = name.toLowerCase().split(' ')
       .map(word => word.charAt(0).toUpperCase() + word.slice(1)).join (' ');
-    const data = await this.reviewerModel.find({ name: { $regex: term, $options: 'i' }}).limit(20);
+
+    const data = await this.reviewerModel.find({ name: { $regex: `^${term}`, $options: 'i' }}).limit(20);
     if (!data || data.length == 0) {
       throw new NotFoundException('Reviewers data not found!');
     }
